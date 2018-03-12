fb7f435539fb1a2f7ef8ef845b05ae88456b385c
kumi <>
2018-03-13 07:40:52 +0900

    データベースにモデル用のテーブルを作るが上手くいかない

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # Create your models here.
-from django.utils import TIME_ZONE
+from django.utils import timezone
 
-class Post(models.Model)
-    author = models.ForeighnKey('auth.User')
-    title = models.Charfield(max_length=200)
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-    default=timezone.now)
+            default=timezone.now)
     published_date = models.DateTimeField(
-    blank=True, null=True)
+            blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,4 +18,3 @@ class Post(models.Model)
 
     def __str__(self):
         return self.title
-    
